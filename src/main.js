a175a3213bb957921b4c02c3e250c55d818ddcf2
fabio thomaz <fabio_thomaz@live.com>
2022-06-25 19:03:57 -0400

    firebase instalado e configurado atraves de variaveis de ambiente

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from '../src/plugins/firebase'
 
+Vue.use(firebase)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,68 +1,79 @@
 <template>
-    <v-row no-gutters justify="center">
-          <v-col sm="4">
-            <h2 class="primary--text text--accent-4">Login</h2>
-            <v-form v-model="valid">
-              <v-row>
-                <v-col cols="12" md="12">
-                  <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="12">
-                  <v-text-field
-                    v-model="firstname"
-                    :rules="nameRules"
-                    label="Senha"
-                    required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row no-gutters>
-                <v-col>
-                  <v-btn
-                    block
-                    elevation="4"
-                    depressed
-                    color="primary"
-                    v-on:click="logged = true"
-                    >Login</v-btn
-                  >
-                </v-col>
-              </v-row>
-              <v-container fluid><v-text class="d-flex justify-center">ou</v-text></v-container>
-
-              <v-row>
-                <v-col>
-                  <v-btn block elevation="4" depressed color="primary"
-                    >Cadastrar</v-btn
-                  >
-                </v-col>
-              </v-row>
-            </v-form>
+  <v-row no-gutters justify="center">
+    <v-col sm="4">
+      <h2 class="primary--text text--accent-4">Login</h2>
+      <v-form v-model="valid">
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field
+              v-model="password"
+              :rules="nameRules"
+              label="Senha"
+              required
+            ></v-text-field>
           </v-col>
         </v-row>
+        <v-row no-gutters>
+          <v-col>
+            <v-btn
+              @click="login()"
+              block
+              elevation="4"
+              depressed
+              color="primary"
+              v-on:click="logged = true"
+              >Login</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-container fluid><p class="d-flex justify-center">ou</p></v-container>
+
+        <v-row>
+          <v-col>
+            <v-btn block elevation="4" depressed color="primary"
+              >Cadastrar</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-    name: "Login",
-    data: () => ({
+  name: "Login",
+  data: () => ({
     valid: true,
     firstname: "",
     lastname: "",
+    password: "",
     nameRules: [
-      (v) => !!v || "Name is required",
-      (v) => v.length <= 10 || "Name must be less than 10 characters",
+      (v) => !!v || "Password é requerido",
+      (v) => v.length >= 6 || "Passwords não pode ter menos de 6 caracteres",
     ],
     email: "",
     emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      (v) => !!v || "E-mail é requerido",
+      (v) => /.+@.+/.test(v) || "E-mail está inválido",
     ],
-    })
-}
+  }),
+  methods: {
+    async login() {
+      const { email, password } = this;
+      try {
+        console.log(this.$firebase)
+        console.log("login");
+      } catch (error) {}
+    },
+  },
+};
 </script>

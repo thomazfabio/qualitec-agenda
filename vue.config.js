69b32a730bbfc55238abe7816b62c5622bb3c14d
fabio thomaz <fabio_thomaz@live.com>
2022-10-26 19:20:04 -0400

    correção de Bug do css scoped

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = defineConfig({
+  configureWebpack:{
+    plugins: [new MiniCssExtractPlugin()],
+  },
   transpileDependencies: [
     'vuetify'
   ]
